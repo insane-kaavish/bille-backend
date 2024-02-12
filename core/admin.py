5331eb1c14409471aca9de7df1f89e5d4225bb0a
Muhammad Azeem Haider <mh06858@st.habib.edu.pk>
2024-02-12 05:01:55 +0500

    sorry forgot to add this

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bill, Room, Appliance, Usage
+from .models import Bill, Room, Appliance, Usage, ContactUs
 
 # Register your models here.
 # admin.site.register(User)
@@ -7,3 +7,4 @@ admin.site.register(Bill)
 admin.site.register(Room)
 admin.site.register(Appliance)
 admin.site.register(Usage)
+admin.site.register(ContactUs)
