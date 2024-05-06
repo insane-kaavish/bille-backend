eef34bdc7c55b6cfdc154b5e8d19253a5cd41150
Ali Asghar <aliasghar.chakera@gmail.com>
2024-05-06 22:14:52 +0500

    prediction conducted alongside scraping

--- core/tasks.py
+++ core/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
+from datetime import datetime
 from .models import CustomUser, Bill
 from .utils.scraper import scrape, read_pdf  # Assuming scrape logic is refactored into a separate file
+from .utils.predict import *
 
 MONTH_CHOICES = [
     (1, 'Jan'),
@@ -16,6 +18,9 @@ MONTH_CHOICES = [
     (11, 'Nov'),
     (12, 'Dec')
 ]
+
+curr_month = datetime.now().month
+curr_year = datetime.now().year
 
 @shared_task
 def scrape_task(user_id):
@@ -39,7 +44,18 @@ def scrape_task(user_id):
                     # create a bill object only if the bill does not exist
                     if not Bill.objects.filter(user=user, month=month, year=year, is_predicted=False).exists():
                         bill = Bill.objects.create(user=user, month=month, year=year, units=unit, is_predicted=False)
-                        bill.save()               
+                        bill.save()
+                curr_bill = Bill.objects.filter(user=user, month=curr_month, year=curr_year, is_predicted=False).first()
+                if curr_bill:
+                    predicted_bill = Bill.objects.filter(user=user, month=curr_month+1, year=curr_year, is_predicted=True).first()
+                    if not predicted_bill:
+                        bill = get_predicted_units(user, curr_month+1, curr_year)
+                        bill = Bill.objects.create(user=user, month=curr_month+1, year=curr_year, units=bill, is_predicted=True)    
+                else:
+                    predicted_bill = Bill.objects.filter(user=user, month=curr_month, year=curr_year, is_predicted=True).first()
+                    if not predicted_bill:
+                        bill = get_predicted_units(user, curr_month, curr_year)
+                        bill = Bill.objects.create(user=user, month=curr_month, year=curr_year, units=bill, is_predicted=True)    
                 return {'status': 200, 'message': 'Scraping successful'}
         return {'status': 500, 'message': 'Scraping failed'}
     except Exception as e:
